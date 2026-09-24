c87c44ed7cd6ee5ffbba85c3310d6f734d05aa5b
sripriya2004-15 <yedlurisripriya6@gmail.com>
2026-09-24 17:36:56 +0530

    Update property service and enable scheduling

--- backend/rental-management-backend/src/main/java/com/rental/rental_management_backend/RentalManagementBackendApplication.java
+++ backend/rental-management-backend/src/main/java/com/rental/rental_management_backend/RentalManagementBackendApplication.java
@@ -2,7 +2,8 @@ package com.rental.rental_management_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableScheduling
 @SpringBootApplication
 public class RentalManagementBackendApplication {
 
--- backend/rental-management-backend/src/main/java/com/rental/rental_management_backend/property/serviceimpl/PropertyServiceImpl.java
+++ backend/rental-management-backend/src/main/java/com/rental/rental_management_backend/property/serviceimpl/PropertyServiceImpl.java
@@ -43,7 +43,7 @@ public class PropertyServiceImpl implements PropertyService {
         this.propertyManagerRepository = propertyManagerRepository;
     }
 
-    @Override
+    @Override 
     public PropertyResponse createProperty(PropertyRequest request) {
 
         User owner = getLoggedInUser();
